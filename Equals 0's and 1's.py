c50b6eb4bab4b3daf483da2abd44cb642e129f0f
Gowni Deepa <gownideepagaaru@gmail.com>
2025-09-20 20:43:44 +0530

    Update Equals 0's and 1's.py

--- Equals 0's and 1's.py
+++ Equals 0's and 1's.py
@@ -1,13 +1,13 @@
 '''Equal 0s and 1s:
 You are given an array of 0's and 1's. Find the length of the longest subarray which has an equal number of 0's and 1's.
 
-Input Format
+Input Format:
 The first line of input contains T - the number of test cases. It is followed by 2T lines, the first line contains N - the size of the array. The second line contains the elements of the array.
 
-Output Format
+Output Format:
 For each test case, print the length of the longest subarray which has equal 0's and 1's, separated by a newline.
 
-Constraints
+Constraints:
 10 points
 1 <= T <= 100
 1 <= N <= 100
@@ -20,7 +20,7 @@ Constraints
 1 <= T <= 1000
 1 <= N <= 104
 
-Example
+Example:
 Input
 2
 5
@@ -58,4 +58,5 @@ for _ in range(int(input())):
         else:
             ans=max(ans,i-d[pf])
        
+
     print(ans)
