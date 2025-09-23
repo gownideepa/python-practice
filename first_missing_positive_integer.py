98567cc94bc4e5271dc4b9dd31914050b44b19f4
Gowni Deepa <gownideepagaaru@gmail.com>
2025-09-23 20:59:05 +0530

    Update first_missing_positive_integer.py

--- first_missing_positive_integer.py
+++ first_missing_positive_integer.py
@@ -1,19 +1,21 @@
-'''First Missing Positive Integer:
+First Missing Positive Integer:
+
+
 You are given an array of integers of size N. Find the first positive integer that is missing from the array.
 
 Note: 
  Try solving in O(N) time without using any additional space, except the input array.  Input Format
 The first line of input contains T - the number of test cases. For each test case, the first line contains one integer N - the size of the array. The second line contains N integers - the elements of the array.
 
-Output Format
+Output Format:
 For each test case, print the first missing positive integer, separated by a newline.
 
-Constraints
+Constraints:
 1 <= T <= 100
 1 <= N <= 105
 -105 <= A[i] <= 105
 
-Example
+Example:
 Input
 2
 5
@@ -27,7 +29,7 @@ Output
 
 Explanation
 
-Example 1: The first missing positive integer is 3, as 1 and 2 are present in the array.'''
+Example 1: The first missing positive integer is 3, as 1 and 2 are present in the array.
 
 
 
@@ -52,4 +54,5 @@ for i in range(t):
     if m==-1:
         print(arr[-1]+1)
     else:
+
         print(m)
